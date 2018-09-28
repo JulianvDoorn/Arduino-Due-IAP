183b3cdc2e8d9ebf7f2d39a403673a6655eeebc6
Julian van Doorn <gigajulian@gmail.com>
2018-09-28 17:07:34 +0200

    Changed hardcoded path to environment var

--- Kvasir.cmake
+++ Kvasir.cmake
@@ -1,3 +1,3 @@
 set(CMAKE_SYSTEM_NAME Generic)
-set(toolchain C:/Users/Julian/kvasir_toolchain)
+set(toolchain $ENV{KVASIR_TOOLCHAIN})
 set(CMAKE_TOOLCHAIN_FILE ${toolchain}/compilers/arm-none-eabi.cmake)
